a62fed874a5a78ebe801df9d1ce86718183cd963
Christian Simon <jamonyroquefort@gmail.com>
2025-09-26 23:14:51 -0300

    Arreglar import de mercadopago

--- pages/api/create-payment.js
+++ pages/api/create-payment.js
@@ -1,4 +1,4 @@
-eexport default async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Solo se permite POST' });
   }
