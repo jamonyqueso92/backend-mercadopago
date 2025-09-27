cdfa1bf69062f09a1ab1d0530bd38e18cabf73c9
Christian Simon <jamonyroquefort@gmail.com>
2025-09-26 22:55:53 -0300

    Usar API directa de MP sin SDK

--- pages/api/create-payment.js
+++ pages/api/create-payment.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+eexport default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Solo se permite POST' });
   }
